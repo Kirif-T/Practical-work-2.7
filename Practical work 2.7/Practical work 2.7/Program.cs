a446a93a74c10d3ceb7a7e0fc43b8f892055da2c
Кирилл Талибаев <163425382+Kirif-T@users.noreply.github.com>
2024-06-03 00:01:13 +0400

    Add files via upload

---
+++ Practical work 2.7/Practical work 2.7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PracticalWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Громогласов Дмитрий Валерьевич";  // Фамилия, имя, отчество персонажа.
+
+            byte age = 29;  // Возраст.
+
+            string email = "gromdmitry94@gmail.com";
+
+            float programmingScores = 37.5F;  // Баллы по программированию.
+            float mathScores = 36.6F;  // Баллы по математике.
+            float physicsScores = 25.0F;  // Баллы по физике.
+
+            string pattern = "Ф.И.О: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5}";
+
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              programmingScores,
+                              mathScores,
+                              physicsScores);
+            Console.ReadKey();
+
+            float totalScores = programmingScores + mathScores + physicsScores;  // Сумма всех баллов.
+            float averageScore = totalScores / 3;  // Среднее арифметическое всех баллов.
+            string newPattern = "Сумма всех баллов: {0} \nСреднее арифметическое всех баллов: {1}";
+
+            Console.WriteLine(newPattern,
+                              totalScores,
+                              averageScore);
+            Console.ReadKey();
+        }
+    }
+}
